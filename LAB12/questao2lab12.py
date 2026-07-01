e35a59c122205681a1320db54f12648c9e6feb4a
lourdess8 <lourdessofia1107@gmail.com>
2026-07-01 15:17:28 -0300

    lab 12 pt1

---
+++ LAB12/questao2lab12.py
@@ -0,0 +1,9 @@
+# função input serve para o usuário inserir uma informação
+input 
+# função range, diz sobre um intervalo/sequencia
+for i in range (3):
+# função abs se refere a um número absoluto
+# função print, imprime uma informação na tela
+    print ("3")
+# função int serve para os números inteiros
+# função break, para o código
---
+++ LAB12/questao3.py
@@ -0,0 +1,6 @@
+valor = input("Digite um valor: ")
+print (int (valor))
+# erro com número composto, pois recebe somente números inteiros
+print (float (valor))
+
+print (bool (valor))
