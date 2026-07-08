21baf4fc850ae4efac28f57bbf6fac340b68cd8c
lourdess8 <lourdessofia1107@gmail.com>
2026-07-08 15:21:54 -0300

    Create questao4.py

---
+++ LAB12/questao4.py
@@ -0,0 +1,6 @@
+n1 = float (input ("insira um número: "))
+n2 =  float (input ("insira outro número: "))
+valor = n1 - n2
+valor_arredondado = round(abs(valor), 2)
+print ("A diferença absoluta desses números", valor_arredondado)
+
